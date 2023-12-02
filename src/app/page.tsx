03280dde48b31c4128ad9e2664640a1c9bb4d735
ailton <atxshared@gmail.com>
2023-12-02 11:05:40 -0300

    Estilizando componente produto com Next Image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,6 @@ export default async function Home() {
   //https://fakestoreapi.com/products
 
   const products = await getProducts();
-  console.log(products);
   return (
     <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
