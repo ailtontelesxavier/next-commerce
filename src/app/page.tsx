ac00419ec813ef12369f2e477161295ff52b37e4
ailton <atxshared@gmail.com>
2023-12-01 23:58:51 -0300

    adicionando navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h3>next e-commerce 14</h3>
-    </main>
+    <div>
+      <h1>next e-commerce 14</h1>
+    </div>
   )
 }
